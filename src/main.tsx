f9d9d0e88998d196ca8026e38e12fcd90e1cc750
Dhruv Tailor <dhruv61998@gmail.com>
2025-02-18 14:16:23 -0600

    Jk it's vite

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { PrimeReactProvider } from 'primereact/api';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { PrimeReactProvider } from "primereact/api";
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <PrimeReactProvider>
-      <App />
+       <App />
     </PrimeReactProvider>
-  </React.StrictMode>,
-);
+  </React.StrictMode>
+)
